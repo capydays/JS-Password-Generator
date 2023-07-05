51d1467307c46ff4064837864f5396d079c52caa
Timothy Olson <tim@Timothys-MacBook-Air-7.local>
2023-07-05 10:25:39 -0500

    Added window promts and confirms for user input on password preferences.

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // Assignment code here
+var numbers = "1234567890"
+var lowerABC = "abcdefghijklmnopqrstuvwxyz"
+var upperABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var specialChar = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~"
 
+var userNum = window.confirm("Would you like your password to contatin numbers?")
+var userLower = window.confirm("Would you like your password to contain lowercase letters?")
+var userUpper = window.confirm("Would you like your password to contain uppercase letters?")
+var userSpecial = window.confirm("Would you like your password to contain special characters?")
+var passLength = window.prompt("How many characters would you like your password to be? (Min 8 - Max 128)")
+
+
+if (!userNum && !userLower && !userUpper && !userSpecial) {
+  window.alert("You have not selected any valid password character options. Please refresh and try again.")
+} else if (parseInt(passLength) >= 8 && parseInt(passLength) <= 128) {
+  window.alert("Your preferences have been saved. Please click the 'Generate Password' button below to generate your unique password.")
+} else {
+  window.alert("You have entered an invalid password length. Please refresh and try again.")
+}
+
+
+
+
+
+// generatePassword() {}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
